023eae86255b82e829a60b91cebe5b0ff1f49426
MarieM <ingmariemelendez@gmail.com>
2023-01-13 23:06:24 -0300

    prueba

--- src/app/components/dashboard/dashboardexperiencia/dashboardexperiencia.component.ts
+++ src/app/components/dashboard/dashboardexperiencia/dashboardexperiencia.component.ts
@@ -12,24 +12,11 @@ import { ExperienciasService } from 'src/app/servicios/experiencias.service';
 export class DashboardexperienciaComponent implements OnInit {
   //Crear e inicializar variables de instancia para almacenar los datos con los que trata el Servicio
   experiencias: Experiencias[]=[];
-  form: FormGroup;
-  id?:number;
 
   constructor(
     //Inyectar el Servicio para tener acceso en la clase a los Métodos
     private http: HttpClient, private ExperienciasService: ExperienciasService,
-    private formBuilder: FormBuilder) {
-      this.form,this.formBuilder.group({
-        logo: [''],
-		    link_instit: [''],
-		    instit: ['',[Validators.required, Validators.minLength(3) ,  Validators.maxLength(450)]],
-		    fecha_inicio: ['',[Validators.required, Validators.minLength(4) ,  Validators.maxLength(20)]],
-		    fecha_fin: [''],
-		    puesto: ['',[Validators.required, Validators.minLength(3) ,  Validators.maxLength(450)]],
-		    descripcion:  ['',[Validators.required, Validators.minLength(3) ,  Validators.maxLength(450)]],
-        id: ['']
-      })
-    }
+    private formBuilder: FormBuilder) { }
     
   listarItems(): void{
     this.ExperienciasService.list().subscribe(data =>{
@@ -38,25 +25,7 @@ export class DashboardexperienciaComponent implements OnInit {
     });
   }
 
-  listarItem(item:any) {
-    this.form.get("logo")?.setValue(item.logo);
-		this.form.get("link_instit")?.setValue(item.link_instit);
-		this.form.get("instit")?.setValue(item.instit);
-		this.form.get("fecha_inicio")?.setValue(item.fecha_inicio);
-		this.form.get("fecha_fin")?.setValue(item.fecha_fin);
-		this.form.get("puesto")?.setValue(item.puesto);
-		this.form.get("descripcion")?.setValue(item.descripcion);
-		this.form.get("id")?.setValue(item.id);
-  }
-
-  guardarItem() {
-    alert("Registro a modificar " + this.form.get("id")?.value);
-  }
-
-  eliminarItem(id:number) {
-    alert("El registro a eliminar es: " + id);
-  }
-
+  
   ngOnInit(): void {
     this.listarItems();
   }
